126fc61bce8d30e40fe793c2eff116587d214f36
Brayden England <bray.patriot@gmail.com>
2021-03-09 15:56:03 -0700

    Remove console logs

--- js/main.js
+++ js/main.js
@@ -11,40 +11,29 @@ openMenu.addEventListener('click', () => {
 
 // menu activation
 let urlString = document.location.href
-console.log(urlString)
 let urlArray = urlString.split('/')
-console.log(urlArray)
 let pageHREF = urlArray[urlArray.length-1]
-console.log(pageHREF)
 let menu = document.querySelectorAll('.normalNav .li a')
-console.log(menu)
 let mobileMenu = document.querySelectorAll('.hiddenNav .li a')
-console.log(mobileMenu)
 
 let matchfound = false
 
 for (let item of menu) {
     let currentURL = item.getAttribute('href')
-    console.log(`${currentURL} === ./${pageHREF}`)
     if (currentURL === './' + pageHREF) {
-        console.log('match!')
         item.parentNode.className = 'li active'
         matchfound = true
     } else {
-        console.log('no match')
         item.parentNode.className = 'li'
     }
 }
 
 for (let item of mobileMenu) {
-    let currentURL = item.getAttribute('href')
     console.log(`${currentURL} === ./${pageHREF}`)
     if (currentURL === './' + pageHREF) {
-        console.log('match!')
         item.parentNode.className = 'li active'
         matchfound = true
     } else {
-        console.log('no match')
         item.parentNode.className = 'li'
     }
 }
