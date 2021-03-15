73d0f07a2ce97d1af09eec5b3967efdb24879327
Brayden England <bray.patriot@gmail.com>
2021-03-15 12:32:12 -0600

    Destinations page + margin update

---
+++ destinations.php
@@ -0,0 +1,50 @@
+<?php $ptitle='Cabot Cruises - Destinations'; include '_header.php'; ?>
+
+<div class="wrapper1200 destinations">
+    <h1>Destinations</h1>
+    <div class="imageArea">
+        <figure class="imageBox">
+            <img src="https://via.placeholder.com/300x270" alt="Destination Image">
+            <figcaption>Caption goes here. Caption goes here. A rea lly nice caption, wow!!! </figcaption>
+        </figure>
+        <figure class="imageBox">
+            <img src="https://via.placeholder.com/300x270" alt="Destination Image">
+            <figcaption>Caption goes here. Caption goes here. A rea lly nice caption, wow!!! </figcaption>
+        </figure>
+        <figure class="imageBox">
+            <img src="https://via.placeholder.com/300x270" alt="Destination Image">
+            <figcaption>Caption goes here. Caption goes here. A rea lly nice caption, wow!!! </figcaption>
+        </figure>
+        <figure class="imageBox">
+            <img src="https://via.placeholder.com/300x270" alt="Destination Image">
+            <figcaption>Caption goes here. Caption goes here. A rea lly nice caption, wow!!! </figcaption>
+        </figure>
+        <figure class="imageBox">
+            <img src="https://via.placeholder.com/300x270" alt="Destination Image">
+            <figcaption>Caption goes here. Caption goes here. A rea lly nice caption, wow!!! </figcaption>
+        </figure>
+        <figure class="imageBox">
+            <img src="https://via.placeholder.com/300x270" alt="Destination Image">
+            <figcaption>Caption goes here. Caption goes here. A rea lly nice caption, wow!!! </figcaption>
+        </figure>
+        <figure class="imageBox">
+            <img src="https://via.placeholder.com/300x270" alt="Destination Image">
+            <figcaption>Caption goes here. Caption goes here. A rea lly nice caption, wow!!! </figcaption>
+        </figure>
+        <figure class="imageBox">
+            <img src="https://via.placeholder.com/300x270" alt="Destination Image">
+            <figcaption>Caption goes here. Caption goes here. A rea lly nice caption, wow!!! </figcaption>
+        </figure>
+        <figure class="imageBox">
+            <img src="https://via.placeholder.com/300x270" alt="Destination Image">
+            <figcaption>Caption goes here. Caption goes here. A rea lly nice caption, wow!!! </figcaption>
+        </figure>
+        <figure class="imageBox">
+            <img src="https://via.placeholder.com/300x270" alt="Destination Image">
+            <figcaption>Caption goes here. Caption goes here. A rea lly nice caption, wow!!! </figcaption>
+        </figure>
+    </div>
+    
+</div>
+
+<?php include '_footer.php'; ?>
