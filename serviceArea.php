5495ea8ef079d43cdf1b942bb4d95ddeb4a5a8e5
Brayden England <bray.patriot@gmail.com>
2021-03-25 17:01:36 -0600

    serviceArea images folder changed, basic code

---
+++ serviceArea.php
@@ -0,0 +1,21 @@
+<?php $ptitle='Cabot Cruises - Service Area'; include '_header.php'; ?>
+
+<div class="serviceArea wrapper1200">
+    <h1>Service Area Images</h1>
+    <div class="serviceAreaImages">
+        <img src="/images/lazy/lazy01.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/lazy/lazy02.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/lazy/lazy03.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/lazy/lazy04.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/lazy/lazy05.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/lazy/lazy06.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/lazy/lazy07.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/lazy/lazy08.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/lazy/lazy09.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/lazy/lazy10.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/lazy/lazy11.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/lazy/lazy12.jpeg" alt="service image" loading="lazy" />
+    </div>
+</div>
+
+<?php include '_footer.php'; ?>
