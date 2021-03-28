eaa774c391f5b8e127777fb944f91e075bde71b1
Brayden England <bray.patriot@gmail.com>
2021-03-28 00:09:00 -0600

    Correct image links and minor styling

--- serviceArea.php
+++ serviceArea.php
@@ -3,18 +3,18 @@
 <div class="serviceArea wrapper1200">
     <h1>Service Area Images</h1>
     <div class="serviceAreaImages">
-        <img src="/images/lazy/lazy01.jpeg" alt="service image" loading="lazy" />
-        <img src="/images/lazy/lazy02.jpeg" alt="service image" loading="lazy" />
-        <img src="/images/lazy/lazy03.jpeg" alt="service image" loading="lazy" />
-        <img src="/images/lazy/lazy04.jpeg" alt="service image" loading="lazy" />
-        <img src="/images/lazy/lazy05.jpeg" alt="service image" loading="lazy" />
-        <img src="/images/lazy/lazy06.jpeg" alt="service image" loading="lazy" />
-        <img src="/images/lazy/lazy07.jpeg" alt="service image" loading="lazy" />
-        <img src="/images/lazy/lazy08.jpeg" alt="service image" loading="lazy" />
-        <img src="/images/lazy/lazy09.jpeg" alt="service image" loading="lazy" />
-        <img src="/images/lazy/lazy10.jpeg" alt="service image" loading="lazy" />
-        <img src="/images/lazy/lazy11.jpeg" alt="service image" loading="lazy" />
-        <img src="/images/lazy/lazy12.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy01.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy02.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy03.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy04.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy05.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy06.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy07.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy08.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy09.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy10.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy11.jpeg" alt="service image" loading="lazy" />
+        <img src="/images/content/lazy/lazy12.jpeg" alt="service image" loading="lazy" />
     </div>
 </div>
 
